f95d619e88231dd8e0cf5e847f97ffd6f906c3bb
Hieu <hieulm.data@gmail.com>
2024-01-06 23:12:04 +0700

    comment update

--- utils/User/post/comment.js
+++ utils/User/post/comment.js
@@ -23,8 +23,8 @@ export const comment = async (data, token) => {
                 return { status: 'failed', message: 'Bạn không có quyền truy cập', code: 401 };
             case 400:
                 return { status: 'failed', message: 'Yêu cầu không hợp lệ', code: 400 };
-            case 200:
-                return { status: 'success', message: 'Bình luận thành công', code: 200 };
+            case 201:
+                return { status: 'success', message: 'Bình luận thành công', code: 201 };
             default:
                 return { status: 'failed', message: 'Lỗi không xác định hoặc máy chủ bảo trì', code: 500 };
         }
